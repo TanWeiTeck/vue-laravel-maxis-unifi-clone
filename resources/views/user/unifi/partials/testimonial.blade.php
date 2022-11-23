97fc9c7e13852df2751c7a327fbe457aefb33e4c
WeiTeckTan <106325397+WeiTeckTan@users.noreply.github.com>
2022-11-23 12:24:30 +0800

    new

--- resources/views/user/unifi/partials/testimonial.blade.php
+++ resources/views/user/unifi/partials/testimonial.blade.php
@@ -58,7 +58,7 @@
             <p class="text-center text-sm">- Mohd Faizal, Kuala Lumpur</p>
         </div>
         <div
-            class="m-3 flex w-[280px] flex-col items-center justify-between rounded-md bg-orange-400 py-4 px-10 shadow-md">
+            class="m-3 flex w-[280px] active flex-col items-center justify-between rounded-md bg-orange-400 py-4 px-10 shadow-md">
             <h3 class="p-2 text-center text-lg italic">"The consultant called me the next day after my application, very
                 efficient."</h3>
             <p class="text-center text-sm">- Chee Siang, Kuala Lumpur</p>
@@ -69,6 +69,48 @@
             <p class="text-center text-sm">- Ariff Othman, Bangi</p>
         </div>
 
+    </slider>
+    <br>
+    <slider>
+        <a href="https://wewe.test/orders/listing"
+            class="mx-0 flex w-1/2 items-center justify-center whitespace-nowrap bg-transparent bg-yellow-300 px-6 py-1 text-sm text-gray-400">
+            All (2)
+        </a>
+        <a href="https://wewe.test/orders/listing/waiting_accept"
+            class="mx-0 flex w-1/2 justify-center whitespace-nowrap bg-transparent px-4 py-1 text-sm text-gray-400">
+            Waiting Accept
+            <div class="flex">(<span class="px-1">0</span>)</div>
+        </a>
+        <a href="https://wewe.test/orders/listing/missing_details"
+            class="mx-0 flex w-1/2 justify-center whitespace-nowrap bg-transparent px-4 py-1 text-sm text-gray-400">
+            Missing Details
+            <div class="flex">(<span class="px-1">0</span>)</div>
+        </a>
+        <a href="https://wewe.test/orders/listing/active"
+            class="mx-0 flex w-1/2 justify-center whitespace-nowrap bg-transparent px-4 py-1 text-sm text-gray-400">
+            Active
+            <div class="flex">(<span class="px-1">1</span>)</div>
+        </a>
+        <a href="https://wewe.test/orders/listing/awaiting_my_review"
+            class="mx-0 flex w-1/2 justify-center whitespace-nowrap bg-transparent px-4 py-1 text-sm text-gray-400">
+            Awaiting My Review
+            <div class="flex">(<span class="px-1">0</span>)</div>
+        </a>
+        <a href="https://wewe.test/orders/listing/delivered"
+            class="mx-0 flex w-1/2 justify-center whitespace-nowrap bg-transparent px-4 py-1 text-sm text-gray-400">
+            Delivered
+            <div class="flex">(<span class="px-1">0</span>)</div>
+        </a>
+        <a href="https://wewe.test/orders/listing/completed"
+            class="mx-0 flex w-1/2 justify-center whitespace-nowrap bg-transparent px-4 py-1 text-sm text-gray-400">
+            Completed
+            <div class="flex">(<span class="px-1">1</span>)</div>
+        </a>
+        <a href="https://wewe.test/orders/listing/cancelled"
+            class="mx-0 flex w-1/2 justify-center whitespace-nowrap bg-transparent px-4 py-1 text-sm text-gray-400">
+            Cancelled
+            <div class="flex">(<span class="px-1">0</span>)</div>
+        </a>
     </slider>
 
 
